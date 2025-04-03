295d52c4e0eda1a0e3ea48b27d5c1ae992a97ef6
Elya-Zam <elyazssousa@gmail.com>
2025-04-03 11:32:49 -0400

    Primeiras attvs

---
+++ exercicio01.js
@@ -0,0 +1,15 @@
+let nome = 'Elyã'
+let idade = '15'
+console.log('Olá, meu nome é', nome, 'e eu tenho', idade, 'anos')
+
+let texto = 'string'
+console.log(texto.toUpperCase())
+console.log(texto.toLocaleLowerCase())
+
+let number = 12
+console.log(typeof number)
+
+let peso = 67
+let altura = 1.63
+let IMC = (peso/(altura**2))
+console.log(IMC)
