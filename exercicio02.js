4b5360ee972c65b6bcd0a8403fa03505b910533c
Elya-Zam <elyazssousa@gmail.com>
2025-04-10 11:39:46 -0400

    Atividade 02

---
+++ exercicio02.js
@@ -0,0 +1,16 @@
+let valo1 = 10
+let valo2 = 15
+console.log(`Soma: ${valo1 + valo2}\nSubetração: ${valo1 - valo2}\nMultiplicação: ${valo1 * valo2}\nDivisão: ${valo1 / valo2}`)
+
+let idd1 = 10
+let idd2 = 15
+let verificação = idd1 > idd2 ?  'Você é mais velho' : 'Você é mais novo'
+console.log(verificação)
+
+let idade = 18
+let maior_idd = idade >= 18 ? 'Você é de Maior' : 'Você é de Menor'
+console.log(maior_idd)
+
+let num = 10
+let ImporPar = num % 2 == 0 ? 'Número Par' : 'Número Impar'
+console.log(ImporPar)
