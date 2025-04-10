c7e5ee50ce9fed35c7dc3b15fc74c9abc714212b
Elya-Zam <elyazssousa@gmail.com>
2025-04-10 10:08:06 -0400

    correção da atividade

--- exercicio01.js
+++ exercicio01.js
@@ -1,8 +1,7 @@
 let nome = 'Elyã'
-let idade = '15'
-console.log('Olá, meu nome é', nome, 'e eu tenho', idade, 'anos')
+let idade = 15
 
-let texto = 'string'
+let texto = 'Olá, meu nome é '+ nome + ' e eu tenho '+ idade+ ' anos'
 console.log(texto.toUpperCase())
 console.log(texto.toLocaleLowerCase())
 
@@ -11,5 +10,5 @@ console.log(typeof number)
 
 let peso = 67
 let altura = 1.63
-let IMC = (peso/(altura**2))
-console.log(IMC)
+let IMC = (peso/(altura**2)).toFixed(2)
+console.log('Seu IMC é '+ IMC)      
